ca6354f8233922b9e51c526c2256f48db3acec41
zloko <notnamejack@ya.ru>
2026-05-02 22:01:27 +0800

    чаты виде компонента

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,11 @@ export default defineConfig({
         appTitle: 'Chats Messenger',
         metaDescription: 'Статическая вёрстка на Vite + TypeScript + SCSS + Handlebars (partials + context).',
         year: new Date().getFullYear(),
+        chats: [
+          { name: 'Андрей', text: 'Изображение', time: '10:49', count: 2 },
+          { name: 'Киноклуб', text: 'стикер', time: '10:49', prefix: 'Вы: ' },
+          { name: 'Илья', text: 'Друзья, у меня...', time: '10:49', count: 4 },
+        ]
       },
     }),
   ],
